a00237e4cba1ff653739529dfce59c3e3eee74b8
Owinda Chulawansa <owinda.chulawansa@gmail.com>
2023-07-10 23:45:31 +0530

    inital commit

---
+++ AIDIALabs.IO/Folder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AIDIALabs.IO
+{
+    public static class Folder
+    {
+        private static Stack<string> pathStack;
+
+        static Folder()
+        {
+            pathStack = new Stack<string>();
+        }
+
+        private static void CreateFolderPath(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    pathStack.Push(path);
+                    DirectoryInfo parent = Directory.GetParent(path);
+                    if (parent != null)
+                    {
+                        CreateFolderPath(parent.FullName);
+                    }
+                }
+                else if (pathStack.Count == 0)
+                {
+                    Directory.CreateDirectory(path);
+                }
+                else
+                {
+                    int count = pathStack.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        Directory.CreateDirectory(pathStack.Pop());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static void CreateFolder(string path)
+        {
+            pathStack.Clear();
+            CreateFolderPath(path);
+        }
+    }
+}
